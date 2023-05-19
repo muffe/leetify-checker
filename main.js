db6d0295842e02975b3bfcd997e81df2a905b8f2
Your Name <you@example.com>
2023-05-19 13:29:24 +0200

    Changed title and menu

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
+      title: 'Leetify Checker',
+      autoHideMenuBar: true,
       webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
         nodeIntegration: true,
